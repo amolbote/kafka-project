c4c3606863d4f86e17a823d8dbb7dfb223005acc
amolbote <amol.a.bote@gmail.com>
2022-03-04 18:53:00 +0530

    added @Profile annotation for AutoConfig

--- src/main/java/com/amol/kafkaproject/config/AutoCreateConfig.java
+++ src/main/java/com/amol/kafkaproject/config/AutoCreateConfig.java
@@ -3,9 +3,12 @@ package com.amol.kafkaproject.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+//This code will be executed only for local profile
+@Profile("local")
 public class AutoCreateConfig {
 
     @Bean
